e70f5af41832cbc385c1068d592f9545442f2f78
Cosmic Arrows <cosmicarrowsllc@Cosmics-MacBook-Pro.local>
2018-05-01 17:21:26 -0400

    Upload assets to Github from hardrive

---
+++ TwoFloats/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  TwoFloats
+//
+//  Created by Laurence Wingo on 12/16/12.
+//  Copyright (c) 2012 Laurence Wingo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]);
+
+{
+
+    float bangersnash;
+    
+    float umperdoodle;
+    
+    bangersnash = 3.14;
+    
+    umperdoodle = 42.0;
+    
+    double charleysChocolateFactory;
+    
+    charleysChocolateFactory = bangersnash + umperdoodle;
+    
+    NSLog(@"The sum of the two for the new candy will be: %.0f!!", charleysChocolateFactory);
+    
+    
+    
+    return 0;
+}
+
